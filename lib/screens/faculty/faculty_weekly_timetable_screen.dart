b5155d0fa198f0d3d611419b7601aa1cc884d745
rahull-prog <rahulbarma2004@users.noreply.github.com>
2026-04-28 15:46:59 +0530

    fix: restore image_picker dependency and add missing import

--- lib/screens/faculty/faculty_weekly_timetable_screen.dart
+++ lib/screens/faculty/faculty_weekly_timetable_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:lucide_icons/lucide_icons.dart';
 
 import 'package:shared_preferences/shared_preferences.dart';
